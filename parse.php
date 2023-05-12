5f6e4e3d91aff9317c051d824e1aa6a770ab5020
maboelhagag <90697334+maboelhagag@users.noreply.github.com>
2023-05-12 09:05:59 +0300

    add parse.php

---
+++ parse.php
@@ -0,0 +1,7 @@
+<?php
+$accessFile = $_POST['access-file'];
+$conn = new PDO("odbc:DRIVER={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=$accessFile; Uid=; Pwd=;");
+$stmt = $conn->query('SELECT * FROM table_name');
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($rows);
+?>
