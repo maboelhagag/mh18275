88e44ca8dc11b2b2347d7ad7d6772c727d85c25e
maboelhagag <90697334+maboelhagag@users.noreply.github.com>
2023-11-16 21:52:51 +0200

    Update parse.php

--- parse.php
+++ parse.php
@@ -1,5 +1,5 @@
 <?php
-$accessFile = $_POST['access-file'];
+$accessFile = $_POST['NW.accdb'];
 $conn = new PDO("odbc:DRIVER={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=$accessFile; Uid=; Pwd=;");
 $stmt = $conn->query('SELECT * FROM Orders');
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
